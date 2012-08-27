e2fcf21738eaf4856eebd43df27327f3b0c13ecb
rocsteady <ccheney@pdx.edu>
2012-08-26 23:12:39 -0700

    Added TODO because there needs to be a better set of arguments passed for searching through the contents of files

--- python/search.py
+++ python/search.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-#Candace Cheney, David Mertz, Steve Holden, Peter Banka, and Kevin Turner
+"""
+ python-term-search
+ No copyright
+
+ This program was constructed by members of the python community.
+
+ Candace Cheney, David Mertz, Steve Holden, Peter Banka and Kevin Turner
+"""
 import sys
+#So that we don't have to use a prefix throughout the program.
 from glob import iglob
 from os.path import isfile
 if len(sys.argv) < 3:
-   print "USAGE: search <path> <search_term>"
-   sys.exit()
+    print "USAGE: search <path> <search_term>"
+#TODO Figure out a way to flip the order of arguments so that it will be easier to search through multiple files. Then, you can use * in the shell and bash will expand that for you and python will get a list of multiple files to search through.
+    sys.exit()
 path, search_term = sys.argv[1:]
 names = iglob(path)
 for name in names:
@@ -14,4 +23,5 @@ for name in names:
     fh = open(name)
     text = fh.read()
     if search_term in text:
-         print "found in", name
+        print "found in", name
+
